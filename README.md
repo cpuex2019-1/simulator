cdbe840e129df1d1ad599f458e40e335df73a1b9
unonao <naokiuno634@gmail.com>
2019-11-11 13:16:20 +0900

    modify 'or' in main

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # simulator for 2nd (mipsim)
-## version 0.1
+## version 0.1.1
 ### how to use
 Make sure you can use `g++` and `c++11`, then, compile the programs:
 ```
--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]) {
                 };
             }
 
-        } else if (str == "e" |
+        } else if (str == "e" ||
                    str ==
                        "execute") { // execute instructions for designated times
 
